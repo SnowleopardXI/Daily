2e0fbadd0513a3c209f2d5513773f87e2222007e
Adam Steve <yjzawall@gmail.com>
2021-09-17 16:49:42 +0800

    mod

--- sipc/class/9.17/test.c
+++ sipc/class/9.17/test.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 int main(void)
 {
-    int in=368;
-    printf("请输入大于100的整数 368\n");
+    int in;
+    printf("请输入大于100的整数\n");
+    scanf("%d",&in);
     printf("The first digit is %d\t",in%10);
     printf("The second digit is %d",in/10%10);
     return 0;
