5a9f45b810d17ebcbe2d30144e2766ac18853ae1
SnowleopardXI <69493681+SnowleopardXI@users.noreply.github.com>
2021-10-26 20:14:33 +0800

    Update Homework.c

--- class/10.26/Homework.c
+++ class/10.26/Homework.c
@@ -8,6 +8,9 @@ int main()
         if (x%2==0)
         sum+=x,j++;
     }
+    if (((float)sum/j)==(sum/j))
+        printf("偶数的和为：%d,其平均值为：%d",sum,sum/j);
+    else
     printf("偶数的和为：%d,其平均值为：%.2f",sum,(float)sum/j);
     return 0;
 }
