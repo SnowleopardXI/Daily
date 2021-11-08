3a55ac5ebc1f322b7e36ca0334bc5e1c6d4aceb4
Steve Ephraim Adam <yjzawall@gmail.com>
2021-11-08 19:37:29 +0800

    59.c

--- sipc/Problems/59.c
+++ sipc/Problems/59.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 int main()
 {
-    __int32_t h,m,s,sec;
-    scanf("%d:%d:%d",&h,&m,&s);
-    scanf("%d",&sec);
-    s=+sec;
-    printf("%d,%d,%d\n",&h,&m,&s);
-    if (s>=60)
-    {
-        s-=60;
-        m++;
-    }
-    if (m>=60)
-    {
-        m-=60;
-        h++;
-    }    
-    printf("%d,%d,%d",&h,&m,&s);
-    return 0;
-}
+  int hour,min,sec0,seci;
+  scanf("%d:%d:%d",&hour,&min,&sec0);
+  scanf("%d",&seci);
+  sec0+=seci;
+  if(sec0>=60)
+  {
+    sec0-=60;
+    min++;
+  }
+  if(min>=60)
+  {
+    min-=60;
+    hour++;
+  }
+  if(hour>=24)
+  {
+    hour-=24;
+  }
+  printf("%02d:%02d:%02d",hour,min,sec0);
+  return 0;
+}}
