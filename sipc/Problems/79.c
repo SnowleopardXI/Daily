0ee76664f5417f786d03b95ec0c8618cad6e5437
SnowleopardXI <69493681+SnowleopardXI@users.noreply.github.com>
2021-11-22 21:17:59 +0800

    Update 79.c

--- sipc/Problems/79.c
+++ sipc/Problems/79.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-long long power(int a,int b)
+long long pow(int a,int b)
 {
 	long long sum=1;
 	for(int i=1;i<=b;i++)
@@ -12,16 +12,17 @@ int main()
 	int a,t,i,n;
 	long long sum=0;
 	scanf("%d",&n);
-	for(i=power(10,n-1);i<=power(10,n)-1;i++)
+	for(i=pow(10,n-1);i<=pow(10,n)-1;i++)
 	{
 		a=i;sum=0;
 		for(int b=1;b<=n;b++)
 		{
 		    t=a%10;
 		    a/=10;
-		    sum=sum+power(t,n);
+		    sum=sum+pow(t,n);
 		}
 		if((int) sum==i)
 		printf("%d\n",i);
 	}
+	return 0;
 }
