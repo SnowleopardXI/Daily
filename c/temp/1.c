73a410964aaa4b7df6387f4f501ff283c6408743
Steve Ephraim Adam <yjzawall@gmail.com>
2022-03-15 23:17:49 +0800

    Daily

--- c/temp/1.c
+++ c/temp/1.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <math.h>
 int main()
 {
-    printf("%d\n",4.5/2.0);
-    printf("%.3f",4.5/2.0);
+    float a, b, c, p, square;
+    printf("Enter the value of a, b, c: ");
+    scanf("%f%f%f", &a, &b, &c);
+    p = (a + b + c) / 2;
+    if (a + b > c && a + c > b && b + c > a)
+    {
+        square = sqrt(p * (p - a) * (p - b) * (p - c));
+        printf("The area of the triangle is %f", square);
+    }
+    else
+    {
+        printf("The given values are not a triangle\n");
+    }
+    return 0;
 }
--- cpp/class/3.15/Homework.cpp
+++ cpp/class/3.15/Homework.cpp
@@ -1,27 +1,25 @@
 #include <iostream>
 using namespace std;
-
 class RationalNumber
 {
 private:
     int a, b; // a为分子，b为分母
 public:
     // construtors
-    RationalNumber();                       //默认值分子为0，分母为1
-    RationalNumber(int m, int n);           //判断如果用户传入分母为0，设置分母为1
+    RationalNumber(); //默认值分子为0，分母为1
+    RationalNumber(int, int); //判断如果用户传入分母为0，设置分母为1
     RationalNumber(const RationalNumber &); //复制构造函数
-                                            // getters
-    int getNumerator();                     //分子
-    int getDenominator();                   //分母
-                                            // setters
-    void setNumerator(int x);               //分子
-    void setDenominator(int y);             //分母,如果分母为0，设置为1
-                                            //其他成员函数
-    void standardize();                     //输出标准化工作，判断如果a和b都是负数，把它们都变成正数；如果a是正b是负，把a变成负b变成正，这样可以使得输出时分母中不出现负号
-    void print();                           //输出a/b的格式，输出前调用standardize()
-    double toDouble();                      //得到a/b的double值，注意不要整除
+    // getters
+    int getNumerator(); //分子
+    int getDenominator(); //分母
+    // setters
+    void setNumerator(int); //分子
+    void setDenominator(int); //分母,如果分母为0，设置为1
+    //其他成员函数
+    void standardize(); //输出标准化工作，判断如果a和b都是负数，把它们都变成正数；如果a是正b是负，把a变成负b变成正，这样可以使得输出时分母中不出现负号
+    void print(); //输出a/b的格式，输出前调用standardize()
+    double toDouble(); //得到a/b的double值，注意不要整除
 };
-
 int main()
 {
     RationalNumber r1, r2(-5, 6);
@@ -46,20 +44,16 @@ int main()
     delete r4;
     return 0;
 }
-
 RationalNumber::RationalNumber()
 {
     a = 0;
     b = 1;
 }
-RationalNumber::RationalNumber(int m, int n)
+RationalNumber::RationalNumber(int numerator, int denominator)
 {
-    a = m;
-    b = n;
-    if (n == 0)
-    {
-        b = 1;
-    }
+    a = numerator;
+    b = denominator;
+    b=b==0?1:b;
 }
 RationalNumber::RationalNumber(const RationalNumber &r)
 {
@@ -74,17 +68,16 @@ int RationalNumber::getDenominator()
 {
     return b;
 }
-void RationalNumber::setNumerator(int x)
+void RationalNumber::setNumerator(int numerator)
 {
-    a = x;
+    a = numerator;
 }
-void RationalNumber::setDenominator(int y)
+void RationalNumber::setDenominator(int denominator)
 {
-    b = y;
-    if (y == 0)
-    {
+    if (denominator == 0)
         b = 1;
-    }
+    else
+        b = denominator;
 }
 void RationalNumber::standardize()
 {
