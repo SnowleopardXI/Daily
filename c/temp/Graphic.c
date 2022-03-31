4d52a5ae2467f5e516204040cd4a1b45d1199e37
Steve Ephraim Adam <yjzawall@gmail.com>
2022-03-31 21:33:30 +0800

    Create Graphic.c

---
+++ c/temp/Graphic.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#define ch '*'
+/*
+int main()
+{
+    int i,column,row,blank;
+    printf("Please input the number of rows and columns:");
+    scanf("%d%d",&row,&column);
+    for(i=1;i<=row;i++)
+    {
+        for(int j=1;j<=column;j++)
+        {
+            printf("%c",ch);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+*/
+/*
+int main()
+{
+    int i,column,row,blank;
+    printf("Please input the number of rows and columns:");
+    scanf("%d%d",&row,&column);
+    for (i = 0; i <=row; i++)
+    {
+        for (int j = 0; j <= i-1; j++)
+        {
+            printf(" ");
+        }
+        for(int k=1;k<=column;k++)
+        {
+            printf("%c",ch);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+*/
+/*
+int main()
+{
+    int i,column,row,blank;
+    printf("Please input the number of rows and columns:");
+    scanf("%d%d",&row,&column);
+    for (i = 0; i < row; i++)
+    {
+        for (int k = 0; k <= i; k++)
+        {
+            printf("%c",ch);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+*/
