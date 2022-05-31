bdfb1418acc39363aca644b63935a5090a53e7c8
Steve Ephraim Adam <yjzawall@gmail.com>
2022-05-31 09:58:30 +0000

    Update 2013.c

--- c/HDUoj/2013.c
+++ c/HDUoj/2013.c
@@ -1,21 +1,16 @@
-/*Problem Description
-
+/*Problem Descriptio
 对于表达式n^2+n+41，当n在（x,y）范围内取整数值时（包括x,y）(-39<=x<y<=50)，判定该表达式的值是否都为素数。
 
 Input
-
 输入数据有多组，每组占一行，由两个整数x，y组成，当x=0,y=0时，表示输入结束，该行不做处理。
 
 Output
-
 对于每个给定范围内的取值，如果表达式的值都为素数，则输出"OK",否则请输出“Sorry”,每组输出占一行。
 
 Sample Input
-
 0 1 0 0
 
 Sample Output
-
 OK
 */
 #include<stdio.h>
