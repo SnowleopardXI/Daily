a888ef19855b53092a34dd07cc5a313dcb876afe
Steve Ephraim Adam <yjzawall@gmail.com>
2022-05-31 10:04:17 +0000

    Update 2005.c

--- c/HDUoj/2005.c
+++ c/HDUoj/2005.c
@@ -1,3 +1,21 @@
+/*
+Problem Description
+给定一个日期，输出这个日期是该年的第几天。
+
+Input
+输入数据有多组，每组占一行，数据格式为YYYY/MM/DD组成，具体参见sample input ,另外，可以向你确保所有的输入数据是合法的。
+
+Output
+对于每组输入数据，输出一行，表示该日期是该年的第几天。
+
+Sample Input
+1985/1/20
+2006/3/12
+
+Sample Output
+20
+71
+*/
 #include <stdio.h>
 
 int is_run(int year)
