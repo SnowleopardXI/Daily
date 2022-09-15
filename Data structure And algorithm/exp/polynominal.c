5bfbb37bec5cbfd154c4d6892b892ce6379e1d49
Steve Ephraim Adam <yjzawall@gmail.com>
2022-09-15 11:49:31 +0800

    Update polynominal.c

--- Data structure And algorithm/exp/polynominal.c
+++ Data structure And algorithm/exp/polynominal.c
@@ -14,6 +14,7 @@ struct polynomial
 {
     Node firstNode;
 }
+//initaluize a polynomial
 void initPolynomial (polynomial *p, monomial *input, int count)
 {
     memset(p->firstNode,0,sizeof(node));
@@ -27,12 +28,41 @@ void initPolynomial (polynomial *p, monomial *input, int count)
         rear->next=NULL;
     }
 }
+//print a polynomial
 void printPolynomial (polynomial *p)
 {
-    Node * rear = p->firstNode->next;
-    while (rear!=NULL)
+    if(firstNode->next==NULL)
+        printf("0\n");
+    Node * theNode=p->firstNode;
+    while(theNode->next!=NULL)
     {
-        printf("%f*x^%d",rear->m.coefficient,rear->m.exponent);
-        rear=rear->next;
+        printf("+%fX^%d",theNode->m.coefficient,theNode->m.exponent);
+        theNode=theNode->next;
+    }
+    printf("\n");
+}
+//free memory
+void freePolynomial (polynomial *p)
+{
+    Node * theNode=p->firstNode;
+    while(p->firstNode->next!=NULL)
+    {
+        theNode=p-firstNode->next;
+        p->firstNode->next=theNode->next;
+        free(theNode);
+    }
+    free(p);
+}
+//add two polynomials
+polynomial * addPolynomial(polynomial * p1,polynomial * p2)
+{
+    polynomial * result=(polynomial *)malloc(sizeof(polynomial));
+    Node *idx1=p1->firstNode->next;
+    Node *idx2=p2->firstNode->next;
+    Node *idx=result->firstNode;
+    while(idx1!=NULL ||idx2!=NULL)
+    {
+        if(idx1==NULL)
+        
     }
 }
