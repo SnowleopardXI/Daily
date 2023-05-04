de48e5e2d221ee4888ac803580baaaf4d11fd0ad
Steve Ephraim Adam <yjzawall@gmail.com>
2023-05-04 22:59:16 +0800

    File

--- Networking/exp/FileClient.java
+++ Networking/exp/FileClient.java
@@ -2,31 +2,107 @@ import java.io.*;
 import java.net.*;
 
 public class FileClient {
+    private static final String SERVER_HOST = "localhost"; // 服务器主机名
+    private static final int SERVER_PORT = 9091; // 服务器端口号
 
-    public static void main(String[] args) throws IOException {
-        int port;
-        String IP;
-        System.out.println("请输入服务器的IP地址");
-        BufferedReader inFromUser1 = new BufferedReader(
-                new InputStreamReader(System.in));// 将流连接到标准输入 inFromUser程序的输入流
-        IP = inFromUser1.readLine();
-        System.out.println("请输入服务器的端口号");
-        BufferedReader inFromUser2 = new BufferedReader(
-                new InputStreamReader(System.in));// 将流连接到标准输入 inFromUser程序的输入流
-        port = Integer.parseInt(inFromUser2.readLine());
-        Socket socket = new Socket(IP, port);
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket(SERVER_HOST, SERVER_PORT); // 创建客户端Socket对象
+            System.out.println("连接到服务器：" + socket.getInetAddress());
 
-        //upload download
-        System.out.println("请输入指令和文件名，用空格隔开");
-        BufferedReader inFromUser3 = new BufferedReader(
-                new InputStreamReader(System.in));// 将流连接到标准输入 inFromUser程序的输入流
-        String command = inFromUser3.readLine();
-        String[] commandArray = command.split(" ");
-        String command1 = commandArray[0];
-        String fileName = commandArray[1];
-        PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-        writer.println(command1);
-        writer.println(fileName);
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in)); // 读取用户输入
+            BufferedReader serverIn = new BufferedReader(new InputStreamReader(socket.getInputStream())); // 读取服务器响应
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true); // 向服务器发送请求
 
+            while (true) {
+                System.out.print("请输入要执行的操作（upload 文件名、download 文件名、list）：");
+                String input = in.readLine(); // 读取用户输入
+
+                if (input == null || input.isEmpty()) {
+                    break;
+                }
+
+                out.println(input); // 发送请求到服务器
+
+                if (input.startsWith("upload ")) { // 处理上传请求
+                    String fileName = input.substring(7);
+
+                    try {
+                        sendFile(fileName, socket); // 上传指定的文件
+                        System.out.println("文件已上传：" + fileName);
+                    } catch (IOException e) {
+                        System.out.println("文件上传失败：" + e.getMessage());
+                    }
+                } else if (input.startsWith("download ")) { // 处理下载请求
+                    String fileName = input.substring(9);
+
+                    try {
+                        receiveFile(fileName, socket); // 下载指定的文件
+                        System.out.println("文件已下载：" + fileName);
+                    } catch (IOException e) {
+                        System.out.println("文件下载失败：" + e.getMessage());
+                    }
+                } else if (input.equals("list")) { // 处理列出目录下文件请求
+                    System.out.println("目录下的文件列表：");
+
+                    while (true) {
+                        String line = serverIn.readLine(); // 读取服务器发送的一行数据
+
+                        if (line.equals(".")) { // 如果收到结束标记，跳出循环
+                            break;
+                        }
+
+                        System.out.println(line); // 输出文件名
+                    }
+                } else {
+                    System.out.println("无效请求：" + input);
+                }
+            }
+
+            socket.close(); // 关闭客户端Socket
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-}
+
+    private static void sendFile(String fileName, Socket socket) throws IOException {
+        File file = new File(fileName);
+
+        if (!file.exists()) { // 如果文件不存在，抛出异常
+            throw new FileNotFoundException("File not found: " + fileName);
+        }
+
+        InputStream in = new FileInputStream(file); // 创建输入流，读取文件数据
+        OutputStream out = socket.getOutputStream(); // 获取服务器输出流
+
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+
+        while ((bytesRead = in.read(buffer)) != -1) { // 不断读取文件数据，直到读取到文件末尾
+            out.write(buffer, 0, bytesRead); // 将数据写入服务器输出流
+        }
+
+        in.close();
+    }
+
+    private static void receiveFile(String fileName, Socket socket) throws IOException {
+        File file = new File(fileName);
+
+        OutputStream out = new FileOutputStream(file); // 创建输出流，写入文件数据
+
+        InputStream in = socket.getInputStream(); // 获取服务器输入流
+    
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+    
+        while ((bytesRead = in.read(buffer)) != -1) { // 不断读取服务器发送的数据，直到读取到结束标记
+            if (bytesRead == 4 && buffer[0] == '.' && buffer[1] == '\r' && buffer[2] == '\n' && buffer[3] == '\r') {
+                break; // 如果读取到结束标记，跳出循环
+            }
+    
+            out.write(buffer, 0, bytesRead); // 将数据写入文件
+        }
+    
+        out.close();
+    }
+}    
--- Networking/exp/FileServer.java
+++ Networking/exp/FileServer.java
@@ -2,71 +2,101 @@ import java.io.*;
 import java.net.*;
 
 public class FileServer {
-    private static final int PORT = 9091;
-    private static final String FILE_PATH = "D:\\temp\\";
+    private static final int PORT = 9091; // 服务器监听的端口号
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(PORT);
-        System.out.println("Server started on port " + PORT);
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT); // 创建服务器端Socket对象
+            System.out.println("服务器已启动，等待客户端连接...");
+            Socket clientSocket = serverSocket.accept(); // 等待客户端连接
+            System.out.println("客户端已连接：" + clientSocket.getInetAddress());
 
-        while (true) {
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Connection from " + clientSocket);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // 读取客户端请求
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true); // 向客户端发送响应
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            // 等待客户端upload download指令
-            String command = reader.readLine();
-            if (command.equals("upload")) {
-                String fileName = reader.readLine();
-                System.out.println("uploading file " + fileName);
-                File file = new File(FILE_PATH + fileName);
-                if (file.exists()) {
-                    System.out.println("File already exists");
-                    PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
-                    writer.println("EXISTS");
-                } else {
-                    PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
-                    writer.println("READY");
-                    FileOutputStream outputStream = new FileOutputStream(file);
-                    InputStream inputStream = clientSocket.getInputStream();
+            while (true) {
+                String request = in.readLine(); // 读取客户端请求
 
-                    byte[] buffer = new byte[4096];
-                    int count;
-                    while ((count = inputStream.read(buffer)) > 0) {
-                        outputStream.write(buffer, 0, count);
-                    }
-
-                    inputStream.close();
-                    outputStream.close();
-                    clientSocket.close();
-                    System.out.println("File " + fileName + " received from client.");
+                if (request == null) {
+                    break;
                 }
-            } else if (command.equals("download")) {
-                String fileName = reader.readLine();
-                File file = new File(FILE_PATH + fileName);
-                if (!file.exists()) {
-                    System.out.println("File not found");
-                    PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
-                    writer.println("NOTEXISTS");
+
+                if (request.startsWith("upload ")) { // 处理上传请求
+                    String fileName = request.substring(7);
+                    receiveFile(fileName, clientSocket);
+                } else if (request.startsWith("download ")) { // 处理下载请求
+                    String fileName = request.substring(9);
+                    sendFile(fileName, clientSocket);
+                } else if (request.equals("list")) { // 处理列出目录下文件请求
+                    listFiles(clientSocket);
                 } else {
-                    PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
-                    writer.println("READY");
-                    FileInputStream inputStream = new FileInputStream(file);
-                    OutputStream outputStream = clientSocket.getOutputStream();
-
-                    byte[] buffer = new byte[4096];
-                    int count;
-                    while ((count = inputStream.read(buffer)) > 0) {
-                        outputStream.write(buffer, 0, count);
-                    }
-
-                    inputStream.close();
-                    outputStream.close();
-                    clientSocket.close();
-                    System.out.println("File " + fileName + " sent to client.");
+                    out.println("Invalid request.");
                 }
             }
-            
+
+            clientSocket.close(); // 关闭客户端Socket
+            serverSocket.close(); // 关闭服务器Socket
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    private static void receiveFile(String fileName, Socket clientSocket) throws IOException {
+        File file = new File(fileName);
+        OutputStream out = new FileOutputStream(file); // 创建输出流，将数据写入到指定的文件中
+        InputStream in = clientSocket.getInputStream(); // 获取客户端输入流
+
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+
+        while ((bytesRead = in.read(buffer)) != -1) { // 不断读取客户端发送的数据，直到读取到结束标记
+            out.write(buffer, 0, bytesRead); // 将数据写入文件
+        }
+
+        out.close();
+        System.out.println("文件已上传：" + fileName);
+    }
+
+    private static void sendFile(String fileName, Socket clientSocket) throws IOException {
+        File file = new File(fileName);
+
+        if (!file.exists()) { // 如果文件不存在，向客户端发送文件不存在的响应
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            out.println("File not found.");
+            return;
+        }
+
+        InputStream in = new FileInputStream(file); // 创建输入流，读取文件数据
+        OutputStream out = clientSocket.getOutputStream(); // 获取客户端输出流
+
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+
+        while ((bytesRead = in.read(buffer)) != -1) { // 不断读取文件数据，直到读取到文件末尾
+            out.write(buffer, 0, bytesRead); // 将数据写入客户端输出流
+        }
+
+        in.close();
+        System.out.println("文件已下载：" + fileName);
+    }
+
+    private static void listFiles(Socket clientSocket) throws IOException {
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        File dir = new File("."); // 获取当前目录
+
+        File[] files = dir.listFiles(); // 获取当前目录下的文件列表
+
+        if (files == null) {
+            return;
+        }
+
+        for (File file : files) {
+            if (file.isFile()) {
+                out.println(file.getName()); // 将文件名发送给客户端
+            }
+        }
+
+        out.println("."); // 发送结束标记，告诉客户端文件列表已经发送完毕
+        System.out.println("已列出目录下的文件");
     }
 }
