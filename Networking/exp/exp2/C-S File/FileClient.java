8b6dd5136350bd7ddefe67044d31864c7c11b500
Steve Ephraim Adam <yjzawall@gmail.com>
2023-05-05 14:44:27 +0800

    Bug fixed

--- Networking/exp/exp2/C-S File/FileClient.java
+++ Networking/exp/exp2/C-S File/FileClient.java
@@ -43,6 +43,12 @@ public class FileClient {
                     String fileName = input.substring(9);
 
                     try {
+                        //判断服务端文件是否存在
+                        String line = serverIn.readLine();
+                        if(line.contains("not")){
+                            System.out.println("文件不存在：" + fileName);
+                            continue;
+                        }
                         receiveFile(fileName, socket); // 下载指定的文件
                         System.out.println("文件已下载：" + fileName);
                     } catch (IOException e) {
