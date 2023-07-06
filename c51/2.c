97705e78fe06b5f985185006dca5f3d67dc6bdba
Steve Ephraim Adam <yjzawall@gmail.com>
2023-07-06 17:23:30 +0800

    Second Semester

--- c51/2.c
+++ c51/2.c
@@ -1,22 +1,30 @@
 #include "reg52.h"
 #include "intrins.h"
+#define KEY1_PRESS  1
+#define KEY2_PRESS  2
+#define KEY3_PRESS  3
+#define KEY4_PRESS  4
+#define KEY_UNPRESS 0
 typedef unsigned int u16;
 typedef unsigned char u8;
 #define NIXIE_A_DP_PORT P0
 #define LED_PORT        P2
+// 定义独立按键控制脚
+sbit KEY1 = P1 ^ 1;
+sbit KEY2 = P1 ^ 0;
+sbit KEY3 = P1 ^ 2;
+sbit KEY4 = P1 ^ 3;
 
-
-u16 id[8]  = {2, 0, 2, 1, 2, 5, 3, 9};
-u16 id1[8] = {2, 0, 2, 1, 2, 5, 4, 5};
-u16 id2[8] = {2, 0, 2, 1, 2, 5, 4, 7};
-
-sbit LSA = P2 ^ 2;
-sbit LSB = P2 ^ 3;
-sbit LSC = P2 ^ 4;
+sbit LSA = P1 ^ 2;
+sbit LSB = P1 ^ 3;
+sbit LSC = P1 ^ 4;
 // 定义74HC595控制管脚
 sbit SRCLK = P3 ^ 6; // 移位寄存器时钟输入
 sbit R0CLK = P3 ^ 5; // 存储寄存器时钟输入
 sbit SER   = P3 ^ 4; // 串行数据输入
+u16 id[8]  = {2, 0, 2, 1, 2, 5, 3, 9};
+u16 id1[8] = {2, 0, 2, 1, 2, 5, 4, 5};
+u16 id2[8] = {2, 0, 2, 1, 2, 5, 4, 7};
 
 void delay_10us(u16 ten_us)
 {
@@ -31,22 +39,8 @@ void delay(u16 ms)
             ;
     }
 }
-
 u16 gsmg_code[17] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07,
                      0x7f, 0x6f, 0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71};
-
-void single_smg(u16 id[10])
-{
-    u16 i = 0, j = 0;
-    LSA = 0;
-    LSB = 0;
-    LSC = 0;
-    for (i = 0; i < 10; i++) {
-        NIXIE_A_DP_PORT = gsmg_code[id[i]];
-        delay_10us(100);
-        NIXIE_A_DP_PORT = 0x00;
-    }
-}
 
 void smg_display(u16 id[8])
 {
@@ -55,45 +49,21 @@ void smg_display(u16 id[8])
         // 选择数码管
         switch (7 - i) {
             case 0:
-                LSA = 0;
-                LSB = 0;
-                LSC = 0;
-                break;
+                LSA = 0;LSB = 0;LSC = 0;break;
             case 1:
-                LSA = 1;
-                LSB = 0;
-                LSC = 0;
-                break;
+                LSA = 1;LSB = 0;LSC = 0;break;
             case 2:
-                LSA = 0;
-                LSB = 1;
-                LSC = 0;
-                break;
+                LSA = 0;LSB = 1;LSC = 0;break;
             case 3:
-                LSA = 1;
-                LSB = 1;
-                LSC = 0;
-                break;
+                LSA = 1;LSB = 1;LSC = 0;break;
             case 4:
-                LSA = 0;
-                LSB = 0;
-                LSC = 1;
-                break;
+                LSA = 0;LSB = 0;LSC = 1;break;
             case 5:
-                LSA = 1;
-                LSB = 0;
-                LSC = 1;
-                break;
+                LSA = 1;LSB = 0;LSC = 1;break;
             case 6:
-                LSA = 0;
-                LSB = 1;
-                LSC = 1;
-                break;
+                LSA = 0;LSB = 1;LSC = 1;break;
             case 7:
-                LSA = 1;
-                LSB = 1;
-                LSC = 1;
-                break;
+                LSA = 1;LSB = 1;LSC = 1;break;
         }
         // 显示数码管
         NIXIE_A_DP_PORT = gsmg_code[id[i]];
@@ -101,39 +71,102 @@ void smg_display(u16 id[8])
         NIXIE_A_DP_PORT = 0x00;
     }
 }
+u8 key_scan(u8 mode)
+{
+    static u8 key = 1;
 
+    if (KEY1 == 0 || KEY2 == 0 || KEY3 == 0 || KEY4 == 0) // 任意按键按下
+    {
+        delay_10us(1000); // 消抖
+        key = 0;
+        if (KEY1 == 0)
+            return KEY1_PRESS;
+        else if (KEY2 == 0)
+            return KEY2_PRESS;
+        else if (KEY3 == 0)
+            return KEY3_PRESS;
+        else if (KEY4 == 0)
+            return KEY4_PRESS;
+    }
+    return KEY_UNPRESS;
+}
+void for_single()
+{
+    u8 i;
+    for (i = 0; i < 8; i++) {
+        LED_PORT = ~(0x01 << i); // 依次点亮LED灯
+        delay(100);              // 延时一段时间
+    }
+}
+void rev_single()
+{
+    u8 i;
+    for (i = 0; i < 8; i++) {
+        LED_PORT = ~(0x80 >> i); // 依次点亮LED灯
+        delay(100);              // 延时一段时间
+    }
+}
+void for_multiple()
+{
+    u8 i;
+    //从右往左点亮多个,依次为1个，2个，3个，4个，5个，6个，7个，8个
+    for (i = 8; i >=1; i--) {
+        LED_PORT = ~(0xff >> i); // 依次点亮LED灯
+        delay(100);              // 延时一段时间
+    }
+    LED_PORT = ~0xff;
+}
+void rev_multiple()
+{
+    u8 i;
+    //从右往左点亮多个,依次为1个，2个，3个，4个，5个，6个，7个，8个
+    for (i = 0; i <= 8; i++) {
+        LED_PORT = ~(0xff >> i); // 依次点亮LED灯
+        delay(100);              // 延时一段时间
+    }
+}
 void main()
 {
-    u8 i = 0, time = 500;
-    u8 count        = 3;
-    NIXIE_A_DP_PORT = 0x00;
-    LSA  = 0;
-    LSB  = 0;
-    LSC  = 0;
+    u8 i = 0, key = 1, temp;
+    unsigned short time = 500;
+    u8 count            = 3;
+    NIXIE_A_DP_PORT     = 0x00;
+    LSA = 0;LSB = 0;LSC = 0;
     delay(1000);
-    while (count) {
-        NIXIE_A_DP_PORT = gsmg_code[count];
-        delay(1000); // 延时1秒
-        count--;     // 数值减1
-    }
+    time = 80;
+    while (time--) {smg_display(id);}
+    time = 80;
+    while (time--) {smg_display(id1);}
+    time = 80;
+    while (time--) {smg_display(id2);}
+    time = 100;
     while (1) {
-        time = 500;
-        while (time--) {
-            smg_display(id);
+        temp = key;
+        for (time = 10000; time > 0; time--) {
+            key = key_scan(0);
+            if (key == 0)
+            {
+                key = temp;
+            }
+            else
+            {
+                break;
+            }
         }
-        time = 500;
-        while (time--) {
-            smg_display(id1);
-        }
-        time = 500;
-		while (time--) {
-			smg_display(id2);
-		}
-		time = 500;
         LED_PORT = ~0x01;
-        for (i = 0; i < 8; i++) {
-            LED_PORT = ~(0x01 << i); // 将1右移i位，然后取反将结果赋值到LED_PORT
-            delay_10us(50000);
+        switch (key) {
+            case 1:
+                for_single();break;
+            case 2:
+                rev_single();break;
+            case 3:
+                for_multiple();
+                break;
+            case 4:
+                rev_multiple();
+                break;
+            default:
+                break;
         }
     }
 }
--- python/hand.py
+++ python/hand.py
@@ -8,25 +8,24 @@ with open('text.txt', 'r', encoding='utf-8') as file:
     text = file.read()
  
 template = Template(
-    background=Image.new(mode="1", size=(2018, 2084), color=1),
+    background=Image.new(mode="1", size=(1700, 1900), color=1),
     font=ImageFont.truetype("kai.ttf", size=60),
-    line_spacing=120,
+    line_spacing=65,
     fill=0,  # 字体“颜色”
     left_margin=0,
     top_margin=0,
     right_margin=0,
     bottom_margin=0,
-    word_spacing=0.08,
-    line_spacing_sigma=1,  # 行间距随机扰动
-    font_size_sigma=2,  # 字体大小随机扰动
-    word_spacing_sigma=1,  # 字间距随机扰动
+    word_spacing=0.05,
+    line_spacing_sigma=0.09,  # 行间距随机扰动
+    font_size_sigma=0.2,  # 字体大小随机扰动
+    word_spacing_sigma=0.4,  # 字间距随机扰动
     end_chars="，。",  # 防止特定字符因排版算法的自动换行而出现在行首
-    perturb_x_sigma=1,  # 笔画横向偏移随机扰动
-    perturb_y_sigma=1,  # 笔画纵向偏移随机扰动
-    perturb_theta_sigma=0.07,  # 笔画旋转偏移随机扰动
+    perturb_x_sigma=0.08,  # 笔画横向偏移随机扰动
+    perturb_y_sigma=0.08,  # 笔画纵向偏移随机扰动
+    perturb_theta_sigma=0.05,  # 笔画旋转偏移随机扰动
 )
 images = handwrite(text, template)
 for i, im in enumerate(images):
     assert isinstance(im, Image.Image)
-    im.show()
-    im.save(r"output.webp".format(i))
+    im.save(os.path.join("output", f"{i}.png"))
